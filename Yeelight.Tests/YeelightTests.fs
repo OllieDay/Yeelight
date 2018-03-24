ffa1d6865ad9d5971d03c5631daaef44ee1e67b3
Ollie Day <OllieDay@users.noreply.github.com>
2018-03-24 18:56:12 +0000

    Add tests for Yeelight.setPower

--- Yeelight.Tests/YeelightTests.fs
+++ Yeelight.Tests/YeelightTests.fs
@@ -7,6 +7,20 @@ module YeelightTests
     let address = IPAddress.Parse "192.168.0.x"
 
     [<Fact>]
+    let ``set power off suddenly`` () =
+        address
+            |> Yeelight.setPower Yeelight.Off Yeelight.Sudden 0
+            |> Async.RunSynchronously
+            |> should equal Yeelight.Ok
+
+    [<Fact>]
+    let ``set power on suddenly`` () =
+        address
+            |> Yeelight.setPower Yeelight.On Yeelight.Sudden 0
+            |> Async.RunSynchronously
+            |> should equal Yeelight.Ok
+
+    [<Fact>]
     let toggle () =
         address
             |> Yeelight.toggle
