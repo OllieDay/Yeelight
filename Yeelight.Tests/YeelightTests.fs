cb3df5f6bdff565ca08cd3d33429e18ae4ba9b9b
Ollie Day <OllieDay@users.noreply.github.com>
2018-03-25 07:49:46 +0100

    Use Theory instead of Fact to reduce repeated code

--- Yeelight.Tests/YeelightTests.fs
+++ Yeelight.Tests/YeelightTests.fs
@@ -41,31 +41,21 @@ module YeelightTests
             |> Async.RunSynchronously
             |> should equal Yeelight.Ok
 
-    [<Fact>]
-    let ``set brightness to 1 suddenly`` () =
+    [<Theory>]
+    [<InlineData 1>]
+    [<InlineData 100>]
+    let ``set brightness suddenly`` brightness =
         address
-            |> Yeelight.setBrightness 1 Yeelight.Sudden 0
+            |> Yeelight.setBrightness brightness Yeelight.Sudden 0
             |> Async.RunSynchronously
             |> should equal Yeelight.Ok
 
-    [<Fact>]
-    let ``set brightness to 100 suddenly`` () =
+    [<Theory>]
+    [<InlineData 1700>]
+    [<InlineData 6500>]
+    let ``set temperature suddenly`` temperature =
         address
-            |> Yeelight.setBrightness 100 Yeelight.Sudden 0
-            |> Async.RunSynchronously
-            |> should equal Yeelight.Ok
-
-    [<Fact>]
-    let ``set temperature to 1700 suddenly`` () =
-        address
-            |> Yeelight.setTemperature 1700 Yeelight.Sudden 0
-            |> Async.RunSynchronously
-            |> should equal Yeelight.Ok
-
-    [<Fact>]
-    let ``set temperature to 6500 suddenly`` () =
-        address
-            |> Yeelight.setTemperature 6500 Yeelight.Sudden 0
+            |> Yeelight.setTemperature temperature Yeelight.Sudden 0
             |> Async.RunSynchronously
             |> should equal Yeelight.Ok
 
